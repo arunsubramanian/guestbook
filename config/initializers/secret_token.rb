45226138d4216b7987e600d890e564b1403b0994
gth824c@gmail.com <arunsubramanian@macintosh-2.gateway.2wire.net>
2013-05-19 17:50:55 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guestbook::Application.config.secret_token = 'bca8d3895aef60348daefd77b68775f0db733ee0032fe216c9cfe30b51d4e483d3730072b01c4174b313219bf2ee2320f0002a2a434f1cbfa04293e0287d845e'
